e822cf006b818fe0ac5b26c1956459f8fb141bf3
Kaloyan Stefanov <kokmarok@gmail.com>
2026-03-25 21:20:44 +0200

    Refactor switch device enable/disable logic

--- devices/switch_device.py
+++ devices/switch_device.py
@@ -1,3 +1,4 @@
+import asyncio
 from typing import cast
 
 from bleak import BleakGATTCharacteristic, normalize_uuid_str
@@ -22,10 +23,12 @@ class SwitchDevice(SwitchEntity):
     async def turn_on(self, **kwargs):
         _LOGGER.debug("Switch device turned on. (%s:%s)", self.coordinator.name, self.coordinator.address)
         await self.coordinator.write_char(self._SWITCH_CHARACTERISTIC, bytes([0x01]))
+        self._attr_is_on = True
 
     async def turn_off(self, **kwargs):
         _LOGGER.debug("Switch device turned off. (%s:%s)", self.coordinator.name, self.coordinator.address)
         await self.coordinator.write_char(self._SWITCH_CHARACTERISTIC, bytes([0x00]))
+        self._attr_is_on = False
 
     async def async_added_to_hass(self):
         self._notification_cb = self._on_ble_notification
@@ -34,7 +37,12 @@ class SwitchDevice(SwitchEntity):
         await self.coordinator.subscribe_char(self._SWITCH_CHARACTERISTIC)
 
         data = await self.coordinator.read_char(self._SWITCH_CHARACTERISTIC)
-        self._attr_is_on = bool(data[0])
+
+        if bool(data[0]):
+            await self.turn_on()
+        else:
+            await self.turn_off()
+
         _LOGGER.debug("Switch device added to hass. (%s:%s)", self.coordinator.name, self.coordinator.address)
 
     async def async_will_remove_from_hass(self):
@@ -44,6 +52,11 @@ class SwitchDevice(SwitchEntity):
     def _on_ble_notification(self, cb_type: CoordinatorCallbackType, char: BleakGATTCharacteristic, data: bytearray):
         _LOGGER.debug("Switch device received BLE notification. Characteristic: %s, Data: %s. (%s:%s)", char, data.hex(), self.coordinator.name, self.coordinator.address)
         if cb_type == CoordinatorCallbackType.NOTIFICATION and char.uuid == self._SWITCH_CHARACTERISTIC:
-            self._attr_is_on = bool(data[0])
+
+            if bool(data[0]):
+                asyncio.ensure_future(self.turn_on())
+            else:
+                asyncio.ensure_future(self.turn_off())
+
             self.coordinator.fire_event({"state": self._attr_is_on})
             self.async_schedule_update_ha_state()
